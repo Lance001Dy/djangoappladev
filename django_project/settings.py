e368307d9bbce63815049a41f4b6058fe5001231
Lance001Dy <ladyer098@gmail.com>
2020-08-06 13:30:43 +0200

    updated Settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,6 +29,8 @@ SECRET_KEY = 'flr$=uux5_(05fz89n2*3qyo2l(dj3c0=e1tfxv436qn5)#dm^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS =['https://djangoappladev.herokuapp.com/']
+
 
 
 
