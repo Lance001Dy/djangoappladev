fa0ab0337c3a84c528cbdf73853fd63029c0d07f
Lance Dyer <66183912+Lance001Dy@users.noreply.github.com>
2022-06-19 15:05:13 +0200

    deploy

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,14 +31,14 @@ SECRET_KEY = 'flr$=uux5_(05fz89n2*3qyo2l(dj3c0=e1tfxv436qn5)#dm^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://djangoapp007.herokuapp.com']
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
-    'users.apps.Usedefinitions
+    'users.apps.UsersConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
